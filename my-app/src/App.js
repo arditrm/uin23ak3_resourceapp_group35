2b555ef5ebd20eacded94f7dbec010f000a1158e
arditrm <ardit.ramadani904@gmail.com>
2023-03-07 15:19:54 +0100

    enkelt htmal og css struktur

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<body>
+    <div class="container">
+    <header>
+<h1>ressursarkiv</h1>
+    </header>
+    <nav>
+        <button onclick="html()" class="Kategori">
+            <h2>html</h2>
+        </button>
+        <button onclick="css()" class="Kategori">
+            <h2>css</h2>
+        </button>
+        <button onclick="javascript()" class="Kategori">
+            <h2>javascript</h2>
+        </button>
+        <button onclick="react()" class="Kategori">
+            <h2>react</h2>
+        </button>
+        <button onclick="headless()" class="Kategori">
+            <h2>sanity of headless cms</h2>
+        </button>
+    </nav>
+    <main id="main">
+        <h2></h2>
+    </main>
+</div>
+      <script src="ressurser.js"></script>
+</body>
+
   );
 }
 
