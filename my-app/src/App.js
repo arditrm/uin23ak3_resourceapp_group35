63dfcdab772cdf8f891722fb882c6ca085272934
arditrm <ardit.ramadani904@gmail.com>
2023-03-09 12:08:42 +0100

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,40 +7,10 @@ import Nav from './components/Nav';
 
 function App() {
   return (
-
-      
-    <body>
-        <div className='container'>
-        <header>
-    <h1>ressursarkiv</h1>
-        </header>
-        <nav>
-            <button onclick="html()" className='Kategori'>
-                <h2>html</h2>
-            </button>
-            <button onclick="css()" className='Kategori'>
-                <h2>css</h2>
-            </button>
-            <button onclick="javascript()" className='Kategori'>
-                <h2>javascript</h2>
-            </button>
-
-            <button onclick="react()" className='Kategori'>
-                <h2>react</h2>
-
-            </button>   
+     <Nav>
             
-            <button onclick="headless()" className='Kategori'>
-
-                <h2>sanity of headless cms</h2>
-            </button>
-        </nav>
-        <main id="main">
-            <h2></h2>
-        </main>
-    </div>
-    </body>
-  );
+        </Nav>
+        );
 }
 
 export default App;
--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,40 +1,49 @@
-import React from "./React"
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 import resources from "./ressurser"
 
 export default function Nav(){
+
+    const handleButtonClick = (category) => {
+        // Filter the resources array to get only the items in the selected category
+        const categoryResources = resources.filter((resource) => resource.category.toLowerCase() === category.toLowerCase());
+        
+        // Set the state with the selected category resources
+        setSelectedResources(categoryResources);
+      };
+      
+      const [selectedResources, setSelectedResources] = useState([]);
+      
     return(
           
-        
+
         <div className='container'>
-        <header>
-    <h1>ressursarkiv</h1>
-        </header>
+      <header>
+        <h1>ressursarkiv</h1>
+      </header>
 
-       <nav>
-            <button onClick="html()" className='Kategori'>
-                <h2>html</h2>
-            </button>
-            <button onClick="css()" className='Kategori'>
-                <h2>css</h2>
-            </button>
-            <button onClick="javascript()" className='Kategori'>
-                <h2>javascript</h2>
-            </button>
+      <nav>
+        <button onClick={() => handleButtonClick("HTML")} className='Kategori'>
+          <h2>HTML</h2>
+        </button>
+        <button onClick={() => handleButtonClick("CSS")} className='Kategori'>
+          <h2>CSS</h2>
+        </button>
+        <button onClick={() => handleButtonClick("JavaScript")} className='Kategori'>
+          <h2>JavaScript</h2>
+        </button>
 
-            <button onClick="react()" className='Kategori'>
-                <h2>react</h2>
-
-            </button>   
+        <button onClick={() => handleButtonClick("React")} className='Kategori'>
+          <h2>React</h2>
+        </button>   
             
-            <button onClick="headless()" className='Kategori'>
-
-                <h2>sanity of headless cms</h2>
-            </button>
-        </nav>
+        <button onClick={() => handleButtonClick("Sanity and headless CMS")} className='Kategori'>
+          <h2>Sanity of headless CMS</h2>
+        </button>
+      </nav>
         <main id="main">
             <h2></h2>
     </main> 
     </div>
+
     )
 }
