143f904b9e7781073c66ba18a28b01eacc9a1ca6
arditrm <ardit.ramadani904@gmail.com>
2023-03-09 12:59:24 +0100

    test

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,49 +1,59 @@
-import React, { useState } from 'react';
-import resources from "./ressurser"
+import React, { useState } from "react";
+import resources from "./ressurser";
 
-export default function Nav(){
+export default function Nav() {
+  const handleButtonClick = (category) => {
+    // Filter the resources array to get only the items in the selected category
+    const categoryResources = resources.filter(
+      (resource) => resource.category.toLowerCase() === category.toLowerCase()
+    );
 
-    const handleButtonClick = (category) => {
-        // Filter the resources array to get only the items in the selected category
-        const categoryResources = resources.filter((resource) => resource.category.toLowerCase() === category.toLowerCase());
-        
-        // Set the state with the selected category resources
-        setSelectedResources(categoryResources);
-      };
-      
-      const [selectedResources, setSelectedResources] = useState([]);
-      
-    return(
-          
+    // Set the state with the selected category resources
+    setSelectedResources(categoryResources);
+  };
 
-        <div className='container'>
+  const [selectedResources, setSelectedResources] = useState([]);
+
+  return (
+    <div className="container">
       <header>
         <h1>ressursarkiv</h1>
       </header>
 
       <nav>
-        <button onClick={() => handleButtonClick("HTML")} className='Kategori'>
+        <button onClick={() => handleButtonClick("html")} className="Kategori">
           <h2>HTML</h2>
         </button>
-        <button onClick={() => handleButtonClick("CSS")} className='Kategori'>
+        <button onClick={() => handleButtonClick("CSS")} className="Kategori">
           <h2>CSS</h2>
         </button>
-        <button onClick={() => handleButtonClick("JavaScript")} className='Kategori'>
+        <button
+          onClick={() => handleButtonClick("JavaScript")}
+          className="Kategori"
+        >
           <h2>JavaScript</h2>
         </button>
 
-        <button onClick={() => handleButtonClick("React")} className='Kategori'>
+        <button onClick={() => handleButtonClick("React")} className="Kategori">
           <h2>React</h2>
-        </button>   
-            
-        <button onClick={() => handleButtonClick("Sanity and headless CMS")} className='Kategori'>
+        </button>
+
+        <button
+          onClick={() => handleButtonClick("Sanity and headless CMS")}
+          className="Kategori"
+        >
           <h2>Sanity of headless CMS</h2>
         </button>
       </nav>
-        <main id="main">
-            <h2></h2>
-    </main> 
-    </div>
 
-    )
-}
+      <main id="main">
+        <h2>Selected Resources</h2>
+        <ul>
+          {selectedResources.map((resource) => (
+            <li key={resource.id}>{resource.title}</li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}   
